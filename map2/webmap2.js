3bff1240a9de1e1fb663665706b38b1045195c8e
Anna M Cook <acook41@lsu.edu>
2019-03-28 11:44:32 -0500

    love at 28

--- map2/webmap2.js
+++
@@ -1,8 +0,0 @@
-let myMap = L.map('map2').setView([32.18, -99.14], 5)
-L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(renameThis)
-L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'NOAA, Iowa State University'
-}).addTo(renameThis)
