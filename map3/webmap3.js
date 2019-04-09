fd7a2b9d4af68084bf4e80e04a79da0e5227c0ba
acook121 <48488050+acook121@users.noreply.github.com>
2019-04-09 10:30:26 -0500

    Add files via upload
    4-9

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -4,20 +4,20 @@ L.tileLayer(basemapUrl).addTo(statemap)
 let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_demographics_ESRI_2010A.geojson'
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   let stateStyle = function (feature) {
-    let age = feature.properties.MED_AGE // get the current state's Median Age attribute
-    let stateColor = 'gold' // let the initial color be a darker green
-    if ( age < 37 ) { stateColor = 'purple' } // if the state's median age is less than the average, color it a lighter green
-    if ( age > 40 ) { stateColor = 'white' }
+    let pop = feature.properties.POPULATION
+    let stateColor = 'white'
+    if ( pop > 1000000 ) { stateColor = 'purple' }
+    if ( pop > 5000000 ) { stateColor = 'gold' }
     return {
-      color: stateColor, //use the color variable above for the value
-      weight: 2.5,
+      color: stateColor,
+      weight: 3,
       fillOpacity: 0.075
     }
   }
   let onEachFeature = function (feature, layer) {
     let name = feature.properties.STATE_NAME
-    let age = feature.properties.MED_AGE
-    layer.bindPopup('Median age of ' + name + ': ' + age + '<br>National average: 38')
+    let pop = feature.properties.POPULATION
+    layer.bindPopup('Population ' + name + ': ' + pop)
   }
   let geojsonOptions = {
     style: stateStyle,
